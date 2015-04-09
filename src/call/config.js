219ca384c77e50538ff5c050d6c3026dfd433985
Xiaojue <designsor@gmail.com>
2015-04-09 16:34:24 +0800

    fix index

--- src/call/config.js
+++ src/call/config.js
@@ -1,3 +1,4 @@
 module.exports = function(ns,debug){
+  var app = ns('app');
   return function(){};
 };
--- src/index.js
+++ src/index.js
@@ -2,18 +2,6 @@ module.exports = function(ns, plugin, debug) {
   plugin.load(__dirname);
   var app = ns('app');
   app.once('start', function() {
-    /*
-    var nav = ns('nav', []);
-    app.call('nav.add', [{
-      url: '/',
-      title: 'index'
-    },{
-      url:'/abc',
-      title:'abc'
-    }], function() {
-      console.log('start');
-    });
-    */
   });
 };
 
